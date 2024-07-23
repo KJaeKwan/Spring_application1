4760971b1f76d3c1af13b8234a09c62ef30b8345
shash0423 <shash042319@naver.com>
2024-07-23 09:58:08 +0900

    실전 예제 1 - 요구사항 분석과 기본 매핑

--- untitled/src/main/java/jpabook/jpashop/JpaMain.java
+++ untitled/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,29 +1,20 @@
-package hellojpa;
+package jpabook.jpashop;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
-public class jpaMain {
+public class JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
         EntityManager em = emf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         try {
-
-            //영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");
-
-            em.flush();
-
-            System.out.println("==========");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
