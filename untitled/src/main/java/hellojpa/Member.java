81e601891ba53b7a7230ff5ac3314a33e0f89805
shash0423 <shash042319@naver.com>
2024-07-26 11:19:46 +0900

    다대다 [N:M]

--- untitled/src/main/java/hellojpa/Member.java
+++ untitled/src/main/java/hellojpa/Member.java
@@ -3,6 +3,9 @@ package hellojpa;
 import jakarta.persistence.*;
 import org.hibernate.annotations.OptimisticLock;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Member {
 
@@ -21,6 +24,9 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
